f22dfb2988b7f80c37e12d64e45a9e1283ff0b88
stepan <sowtame@mail.ru>
2020-03-01 20:12:59 +0700

    Updates

---
+++ precache-manifest.67c3dbcfddc3d836e04a620b34faa8c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915a1a2acf45ac125981073d7b857e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c87cc1ac41a247eda66f",
+    "url": "/static/css/2.ba56cd67.chunk.css"
+  },
+  {
+    "revision": "7abc9d2a14ca3d911ac1",
+    "url": "/static/css/main.a1d93b4d.chunk.css"
+  },
+  {
+    "revision": "c87cc1ac41a247eda66f",
+    "url": "/static/js/2.bb0eff35.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.bb0eff35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7abc9d2a14ca3d911ac1",
+    "url": "/static/js/main.eedf035d.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "0c89403d9f4c4a2dc42e90b2ea448acf",
+    "url": "/static/media/news.0c89403d.svg"
+  }
+]);
